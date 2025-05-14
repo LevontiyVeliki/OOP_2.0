599defb1a3cc06ea66fdd2705fdabbfd53de8340
LevontiyVeliki <151672905+LevontiyVeliki@users.noreply.github.com>
2025-05-14 13:16:05 +0300

    Add files via upload

---
+++ C++/Задание 9/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <map>
+
+#include "lib/myadapters.h"
+
+int main() {
+  std::vector<int> v = {1, 2, 3, 4, 5, 6};
+  std::vector<int> r2 = {4, 5};
+  auto r = v | Transform([](int i){
+    return i * i;}) | Transform([](int i){
+    return i * 3;});
+  for (auto i : r){
+    std::cout << i << ' ';
+  }
+
+  return 0;
+}
